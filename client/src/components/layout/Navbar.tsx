856e5ae86f5d851cb6582363ab7e61b91b4eb623
Adityaraj1612 <adityaraj10b@gmail.com>
2025-06-18 16:43:17 +0530

    change header background color from white to red in Navbar

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="bg-white shadow-sm sticky top-0 z-50">
+    <header className="bg-red-500 shadow-sm sticky top-0 z-50">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <Link href="/">
           <div className="flex items-center cursor-pointer">
